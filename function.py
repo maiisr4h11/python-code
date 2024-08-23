c228151d11944142969ef70f2c10b0661e5607cc
maiisr4h11 <maisarahibrahim2511@gmail.com>
2024-08-23 09:51:07 +0800

    add changes

--- function.py
+++ function.py
@@ -7,6 +7,12 @@
 # greet() #calling a function
 # print("bye")
 
-def greet(name):
-    print (f"Hello {name}")
-greet("Sarah")
+# def greet(name):
+#     print (f"Hello {name}")
+# greet("Sarah")
+
+def greet(name = "sarah"):
+    print(f"hello {name}")
+
+greet()
+greet("Mike")
