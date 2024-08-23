f963406e33453d52edc8bab3b41d9c6ec9f84d09
maiisr4h11 <maisarahibrahim2511@gmail.com>
2024-08-23 09:45:08 +0800

    new python file is added

--- breakandcontinue.py
+++ breakandcontinue.py
@@ -1,32 +1,32 @@
 # break and continue
 
-# for i in range (1,11):
-#     print (i)
-#     if i == 5:
-#         print("The loop is stopped")
-#         break
+for i in range (1,11):
+    print (i)
+    if i == 5:
+        print("The loop is stopped")
+        break
 
-# count = 0
-# while True:
-#     count+=1
-#     print(count)
-#     if count == 7:
-#         print ("The loop is stopped")
-#         break
+count = 0
+while True:
+    count+=1
+    print(count)
+    if count == 7:
+        print ("The loop is stopped")
+        break
 
 #continue statement
 
-# for i in range (1,11):
-#     if i == 6:
-#         continue
-#     print ("The value of i is :", i)
+for i in range (1,11):
+    if i == 6:
+        continue
+    print ("The value of i is :", i)
 
 #string example
-# word = "python"
-# for i in word:
-#     if i == "h":
-#         continue # Skip the rest of the loop when the letter is 'h'
-#     print(i)
+word = "python"
+for i in word:
+    if i == "h":
+        continue # Skip the rest of the loop when the letter is 'h'
+    print(i)
 
 #array example
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
---
+++ function.py
@@ -0,0 +1,12 @@
+#defining a function
+# def greet(): 
+#     print("Hello")
+#     print("How do you do")
+#     print("Isn't the weather nice today?")
+
+# greet() #calling a function
+# print("bye")
+
+def greet(name):
+    print (f"Hello {name}")
+greet("Sarah")
