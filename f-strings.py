1f77d136b7f73839d5ceb8e89f3e5902368b1f2a
maiisr4h11 <maisarahibrahim2511@gmail.com>
2024-08-26 09:50:30 +0800

    f-strings and tuples

---
+++ f-strings.py
@@ -0,0 +1,9 @@
+#f-strings in Python
+# name = "Sarah"
+# age = "23"
+# greeting = f"Hello, my name is {name} and I am {age} years old." #f-strings is for string concatenation
+# print(greeting)
+
+a = 5
+b = 10
+print(f"The sum of {a} and {b} is {a+b}")
